2a81a00802bdf9b1db47663bee1881187de6be02
SEC\user <dorothy_007@naver.com>
2019-05-25 23:00:29 +0900

    state 배열 나열
    map 함수 사용

--- src/Ani.js
+++ src/Ani.js
@@ -4,21 +4,29 @@ import './Ani.css';
 class Ani extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      items: ['hello', 'world', 'click', 'me']
+    }
   }
 
   render() {
+    const { items } = this.state;
+    const view = items.map((value, index) => {
+      return <li key={index}>{value}</li>
+    });
+
     return(
       <div className="ani-wrapper">
         <input
           type="text"
           placeholder="작성하세요."
         />
-      <button>등록</button>
-      <ul>
-        <li>Test</li>
-        <li>Test</li>
-        <li>Test</li>
-      </ul>
+
+        <button>등록</button>
+
+        <ul>
+          {view}
+        </ul>
       </div>
     );
   }
